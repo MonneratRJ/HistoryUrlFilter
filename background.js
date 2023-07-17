9e66e4bc0a4a8103900ada262b6ac7289415ea53
MonneratRJ <marcelo.monnerat@haytek.com.br>
2023-07-17 17:38:20 -0300

    Changed the way it triggers to webNavigation

--- background.js
+++ background.js
@@ -10,18 +10,20 @@ function retrieveData() {
   });
 }
 
-async function searchForMatches() {
+async function cleanHistory() {
   let wordArray;
 
   try {
     wordArray = await retrieveData();
   } catch (error) {
-    console.log(error);
-    return; // Exit the function if there was an error
+    // Exit the function if there was an error
+    console.error(error);
+    return;
   }
 
   if (wordArray) {
     wordArray.forEach(element => {
+      // Basic history cleaning
       chrome.history.search({ text: element }).then(function (results) {
         for (let k in results) {
           let onDelete = chrome.history.deleteUrl({ url: results[k].url });
@@ -32,6 +34,22 @@ async function searchForMatches() {
   }
 }
 
-chrome.history.onVisited.addListener(() => {
-  searchForMatches();
+async function checkForOccurrences(url) {
+  let wordArray;
+
+  try {
+    wordArray = await retrieveData();
+  } catch (error) {
+    // Exit the function if there was an error
+    console.error(error);
+    return;
+  }
+
+  return wordArray.some(word => url.includes(word));
+}
+
+chrome.webNavigation.onCommitted.addListener(async function (details) {
+  if (details.url && await checkForOccurrences(details.url)) {
+    cleanHistory();
+  }
 });
